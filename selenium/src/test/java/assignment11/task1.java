d14fb399fc253936ba6fff2ae9875fc006fa139c
pratiksha.nigade <pratiksha.nigade@gupshup.io>
2023-04-08 20:12:49 +0530

    pratiksha

--- selenium/src/test/java/assignment11/task1.java
+++ selenium/src/test/java/assignment11/task1.java
@@ -25,7 +25,7 @@ public class task1 {
 		driver.findElement(By.xpath("//button[@onclick='timedText()']")).click();
 		value = helper.verifytexttobepresent("//p[@id='demo']", "WebDriver");
 		System.out.println(value);
-		System.out.println("commit from pratiksha branch");
+		System.out.println("git pratiksh");
 
 
 	}
